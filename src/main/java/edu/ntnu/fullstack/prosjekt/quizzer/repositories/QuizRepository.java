22d316e7fbf69d9854fc3294a70d4e348ca5a171
SkinnyAG <andreas.gjersoe@hotmail.no>
2024-04-04 14:41:07 +0200

    Added javadoc for Repository classes

--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/repositories/QuizRepository.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/repositories/QuizRepository.java
@@ -4,6 +4,9 @@ import edu.ntnu.fullstack.prosjekt.quizzer.domain.entities.QuizEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Provides basic CRUD functionality for database operations against the quiz database table.
+ */
 @Repository
 public interface QuizRepository extends CrudRepository<QuizEntity, Long> {
 }
