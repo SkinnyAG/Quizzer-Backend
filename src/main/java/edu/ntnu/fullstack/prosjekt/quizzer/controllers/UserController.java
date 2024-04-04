8d393651ff3047d5460a6d4b645f7be4db322b2c
SkinnyAG <andreas.gjersoe@hotmail.no>
2024-04-04 17:48:10 +0200

    Fixed suggestions
    Changed user already exists error status code, added a simple log when receiving a register request, hiding password when sending userDto response

--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/controllers/UserController.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/controllers/UserController.java
@@ -6,6 +6,8 @@ import edu.ntnu.fullstack.prosjekt.quizzer.domain.entities.UserEntity;
 import edu.ntnu.fullstack.prosjekt.quizzer.mappers.Mapper;
 import edu.ntnu.fullstack.prosjekt.quizzer.services.UserService;
 import java.util.Optional;
+
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Rest Controller used for managing requests relating to user database operations.
  * Base endpoint is /api/users/
  */
+@Log
 @CrossOrigin(origins = "http://localhost:5174")
 @RequestMapping("/api/users")
 @RestController
@@ -60,6 +63,7 @@ public class UserController {
    */
   @PostMapping(path = "/register")
   public ResponseEntity<?> createUser(@RequestBody UserDto user) {
+    log.info("Received register request from user: " + user);
     try {
       UserEntity userEntity = userMapper.mapFrom(user);
       //Check if user already exists
