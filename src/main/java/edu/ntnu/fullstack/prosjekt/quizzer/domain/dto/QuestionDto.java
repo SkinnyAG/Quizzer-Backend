a22766ead2fb195dd0b00c9e4d736fbbcbb6cf56
SkinnyAG <andreas.gjersoe@hotmail.no>
2024-04-04 14:19:00 +0200

    Javadoc and Checkstyle for Dtos
    Added javadoc and complyiing with checkstyle for dtos

--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/QuestionDto.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/QuestionDto.java
@@ -5,17 +5,41 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * The UserDto class is a mirror of the userEntity class, with the intention of creating
+ * a separation between user input/output and database objects.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class QuestionDto {
-    private Long questionId;
+  /**
+   * The questionId field is a unique identifier and primary key for a question entry in
+   * the database.
+   */
+  private Long questionId;
 
-    private String label;
+  /**
+   * The label field represents the question that is being asked, such as "How many people live
+   * in Norway?."
+   */
+  private String label;
 
-    private Short position;
+  /**
+   * The position field should tell the question which position it will have in an ordered quiz,
+   * which will be randomized if wanted.
+   */
+  private Short position;
 
-    private String alternatives;
+  /**
+   * The alternatives field should store JSON with the different alternatives that
+   * a user can answer.
+   */
+  private String alternatives;
 
-    private QuizEntity quiz;
+  /**
+   * The quiz field references which quiz the question belongs to, in a many questions to
+   * one quiz relationship.
+   */
+  private QuizEntity quiz;
 }
--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/QuizDto.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/QuizDto.java
@@ -1,24 +1,37 @@
 package edu.ntnu.fullstack.prosjekt.quizzer.domain.dto;
 
-import edu.ntnu.fullstack.prosjekt.quizzer.domain.entities.QuestionEntity;
 import edu.ntnu.fullstack.prosjekt.quizzer.domain.entities.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
+/**
+ * The QuizDto class is a mirror of the QuizEntity class, with the intention of creating
+ *  * a separation between user input/output and database objects.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class QuizDto {
-    private Long quizId;
+  /**
+   * The quizId field is a unique identifier and primary key for a quiz entry in
+   * the database.
+   */
+  private Long quizId;
 
-    private String title;
+  /**
+   * The title field represents the name of the quiz.
+   */
+  private String title;
 
-    private String description;
+  /**
+   * The description field should be used for describing the quiz.
+   */
+  private String description;
 
-    private UserEntity owner;
-
-    private List<QuestionEntity> questions;
+  /**
+   * The owner field should represent the user who owns the quiz, with many quizzes belonging to
+   * one user. This field is a foreign key linking owner and quizzes.
+   */
+  private UserEntity owner;
 }
