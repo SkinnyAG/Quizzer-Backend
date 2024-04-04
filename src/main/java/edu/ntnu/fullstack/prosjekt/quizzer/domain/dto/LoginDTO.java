ab97a2241731697e136f12be17c4c03e3392c318
Honrik <henrikgnilsen02@gmail.com>
2024-04-04 11:08:04 +0200

    Refactored loginDTO to use @data instead of creating all getters and setters

--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/LoginDTO.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/LoginDTO.java
@@ -1,43 +1,15 @@
 package edu.ntnu.fullstack.prosjekt.quizzer.domain.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
   private String username;
 
-  private String fullName;
-
-  private String email;
-
   private String password;
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getFullName() {
-    return fullName;
-  }
-
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
