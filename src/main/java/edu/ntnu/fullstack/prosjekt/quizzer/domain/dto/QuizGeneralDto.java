69b945ef87e64aae44caa63d04a28c629da40352
SkinnyAG <andreas.gjersoe@hotmail.no>
2024-04-06 10:04:03 +0200

    Different quiz dto objects
    Created two different quiz dtos based on controller request

---
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/dto/QuizGeneralDto.java
@@ -0,0 +1,26 @@
+package edu.ntnu.fullstack.prosjekt.quizzer.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Dto returned from controller containing basic quiz information such as
+ * title, description, owner data, image link and quiz id.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuizGeneralDto {
+  private Long quizId;
+
+  private String title;
+
+  private String description;
+
+  private String imageLink;
+
+  private Long amountOfQuestions;
+
+  private UserDto owner;
+}
