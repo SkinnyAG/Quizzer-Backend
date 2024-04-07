99f183d74dfc30c5d75787f23a3a99a931918ecc
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-07 11:58:41 +0200

    increased max answer amount

--- src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/entities/QuestionEntity.java
+++ src/main/java/edu/ntnu/fullstack/prosjekt/quizzer/domain/entities/QuestionEntity.java
@@ -1,13 +1,7 @@
 package edu.ntnu.fullstack.prosjekt.quizzer.domain.entities;
 
 import edu.ntnu.fullstack.prosjekt.quizzer.enums.QuestionType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -54,6 +48,7 @@ public class QuestionEntity {
    * The alternatives field should store JSON with the different alternatives that
    * a user can answer.
    */
+  @Column(columnDefinition = "longtext")
   private String alternatives;
 
   /**
